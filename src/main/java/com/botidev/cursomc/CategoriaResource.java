f0d7ebbf8d4dfcc9596fc6ebb56bca5fe661a8e1
Renato Botini <botinirenato@gmail.com>
2021-12-17 12:20:05 -0300

    Testando a Classe de Dominio -Categoria

--- src/main/java/com/botidev/cursomc/CategoriaResource.java
+++ src/main/java/com/botidev/cursomc/CategoriaResource.java
@@ -1,7 +1,13 @@
 package com.botidev.cursomc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.botidev.cursomc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
@@ -11,8 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriaResource {
 	
 @RequestMapping(method = RequestMethod.GET)
-public String Listar() {
-	return "Rest está Funcionando";
+public List<Categoria> listar() {
+	
+	Categoria cat1 = new Categoria(1,"Informatica");
+	Categoria cat2 = new Categoria(2,"Papelaria");
+	Categoria cat3 = new Categoria(3,"Diversos");
+	
+	
+	List<Categoria> lista = new ArrayList<>();
+	lista.add(cat1);
+	lista.add(cat2);
+	lista.add(cat3);
+	
+	return lista;
+	
 }
 
 
